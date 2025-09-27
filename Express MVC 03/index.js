9fdccedcc82986a5ebf7e812eafcaec3f22e7af0
yashrajyadav2002 <yyash9631@gmail.com>
2025-09-27 23:38:36 +0530

    update

--- Express MVC 03/index.js
+++ Express MVC 03/index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const studentRoute= require("./routes/student");
+const bodyparser = require('body-parser');
 
-mongoose.connect("mongodb://127.0.0.1:27017/yadavji").then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017//yadavji").then(()=>{
     console.log("DB is connected ....")
 })
 
-app.set("view engine","ejs");
+// Body-parser middleware
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json());
 
+app.set("view engine","ejs");
 app.use("/student", studentRoute);
 
 
 app.listen(7000,()=>{
-    console.log("server is running port on 7000")
+    console.log("server is running on port 7000 .. !!!!1")
 })
