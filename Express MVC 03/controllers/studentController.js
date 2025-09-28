ebe741c5baf6fbb7f3888feb4bd31febc06a7133
yashrajyadav2002 <yyash9631@gmail.com>
2025-09-28 13:54:23 +0530

    ad

--- Express MVC 03/controllers/studentController.js
+++ Express MVC 03/controllers/studentController.js
@@ -1,4 +1,5 @@
 
+const studentModels = require("../models/studentModels");
 const stuModel = require("../models/studentModels");
 const homepage=(req,res)=>{
     res.render("home");
@@ -18,6 +19,18 @@ const contactpage=(req,res)=>{
 
 const salarypage=(req,res)=>{
     res.render("salary");
+}
+
+const datasave=(req,res)=>{
+    const{rno,nm,city,fs}=req.body;
+    studentModels.create({
+        rollno:rno,
+        name:nm,
+        city:city,
+        fees:fs
+    })
+    res.render("insert");
+
 }
 
 module.exports ={
