468fddf6709faaec90304eaffd0bbe35dc715e44
yashrajyadav2002 <yyash9631@gmail.com>
2025-10-21 23:07:15 +0530

    reapeat

--- re node 02/index.js
+++ re node 02/index.js
@@ -81,3 +81,91 @@
 //     if(err)throw err;
 //     console.log("files in directory ",files);
 // })
+
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! REAPEAT !!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// const http = require("http");
+// const fs= require("fs");
+
+
+// http.createServer((req,res)=>{
+//     res.write("<h1>welocome to my file</h1>")
+//     fs.readFile("raj.txt",(err,data)=>{
+//         if(err) throw err
+//         res.write(data);
+//         res.end("server end !!!!!!!1");
+//     })
+// }).listen(5000,()=>{
+//     console.log("server is running...")
+// })
+
+
+// !!!!!!!!!!!!!!!!!!!!! file is crated (this is change and replace the data in write file ) !!!!!!!!!!!!!!1
+
+// const fs = require("fs");
+
+// fs.writeFile("raju.txt","welcome to my new file raju bhai",(err)=>{
+//     if(err)throw err;
+//     console.log("file is crated")
+// })
+
+// !!!!!!!!!!!!!!!!!!!!!!!! apend file is not replace the data  !!!!!!!!!!
+
+
+// const fs =require("fs");
+// fs.appendFile("ranu.txt","hello this is now it is changed new file",(err)=>{
+//     if(err)throw err;
+//     console.log("file is created")
+
+// })
+
+// !!!!!!!!!!!!!!!!!!!!!1 in this we will rename the file (rename) !!!!!!!!!!!!
+
+// const fs = require("fs");
+
+// fs.rename("ramesh.txt","ramesh.pdf",(err)=>{
+//     if(err)throw err;
+//     console.log("file name is changed");
+// });
+
+// !!!!!!!!!!!!!! in the (unlink ) we can delete the file !!!!!!!!!!1
+
+// const fs =require("fs");
+// fs.unlink("raju.txt",(err)=>{
+//     if(err)throw err;
+//     console.log("file is deleted.....")
+// })
+
+
+// !!!!!!!!!!!!!!!! exists is find the file !!!!!!!!!!!
+
+// const fs =require("fs");
+
+// if (fs.existsSync("eximple.txt")){
+//     console.log("file hai")
+// }
+// else{
+//     console.log("file naho hai")
+// }
+
+
+// !!!!!!!!!!!!!!!!! mkdir is craete  new the folders !!!!!!!!!!!!
+
+// const fs = require("fs");
+
+// fs.mkdir("bhopal",(err)=>{
+//     if(err) throw err;
+//     console.log("new directory is created!!!!")
+// })
+
+
+// !!!!!!!!!!!!!!! readdir is read the current directory !!!!!!!!!!!!!!!!!
+
+// const fs = require("fs");
+
+// fs.readdir("./bhopal",(err,files)=>{
+//     if(err)throw err;
+//     console.log("files in directory ",files);
+// })
