d7ebe90b06e56b54d98857d047b2ca3aa9d7bdea
yashrajyadav2002 <yyash9631@gmail.com>
2025-10-22 23:08:40 +0530

    runnning

--- Eexpress CURD 07/server/index.js
+++ Eexpress CURD 07/server/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+const cors=require("cors");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const stuRoute=require("./routes/studentRoute");
+require('dotenv').config();
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/student",stuRoute);
+mongoose.connect(process.env.DBCONNECTION).then(() => console.log("connected to db"));
 
-app.listen(8000,()=>{
-    console.log("serer is running on port 8000..")
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log("Server running on port 3001");
 })
--- Eexpress CURD 07/server/routes/studentRoute.js
+++ Eexpress CURD 07/server/routes/studentRoute.js
@@ -1,4 +1,13 @@
-const express = require("express");
-const route = express.Router();
+const express=require("express");
+const stucontroller = require("../controllers/studentController");
+const router=express.Router();
 
-route.send("/home",)
+router.post("/create",stucontroller.createStudent);
+router.get("/display",stucontroller.getAllData);
+router.post("/search",stucontroller.searchData);
+router.get("/delete/:id",stucontroller.deleteData);
+router.get("/show/:id",stucontroller.getStudent);
+router.post("/updatestu",stucontroller.updateStudent);
+
+
+module.exports=router;
