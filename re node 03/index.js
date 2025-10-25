40da61ac0ea8d3fdcc03c48202e513b488ba24e7
yashrajyadav2002 <yyash9631@gmail.com>
2025-10-25 23:37:47 +0530

    new

--- re node 03/index.js
+++ re node 03/index.js
@@ -1,6 +1,8 @@
 const http = require("http");
 http.createServer((req,res)=>{
     res.write("<h1> welcome to my website friends </h1>");
+    res.write("<p> This is my pera for my personal information </p> ");
+    res.write("<h2> For more details contact me </h2>")
     res.end("server is end !!!!");
 }).listen(4400,()=>{
     console.log("server is running on port 4400")
