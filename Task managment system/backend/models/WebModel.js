4b56100b997f615c726f0d29c341852d462abe62
yashrajyadav2002 <yyash9631@gmail.com>
2025-10-30 23:02:28 +0530

    taskmanagment

---
+++ Task managment system/backend/models/WebModel.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+const webSchema = new mongoose.Schema();
+
---
+++ Task managment system/frontend/src/Components/Home.jsx
@@ -0,0 +1,9 @@
+const HomePage = ()=>{
+    return(
+        <>
+        <h1>This is my home page</h1>
+        </>
+    )
+}
+
+export default HomePage;
